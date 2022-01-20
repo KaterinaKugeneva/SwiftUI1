7d6ac1054e2f86e3eff92ab034c396d96c10dbfb
KaterinaKugeneva <kugeneva1@gmail.com>
2022-01-20 10:55:31 +0300

    Initial Commit

---
+++ SwiftUI1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI1
+//
+//  Created by Ekaterina Kugeneva on 20.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI1/SwiftUI1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI1App.swift
+//  SwiftUI1
+//
+//  Created by Ekaterina Kugeneva on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
